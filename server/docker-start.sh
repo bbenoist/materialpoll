d4551f907500680a31fca26d0d1068178800371e
Baptist BENOIST <baptist.benoist@ubisoft.com>
2017-07-27 23:03:08 +0200

    Fix

--- server/docker-start.sh
+++ server/docker-start.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+set -e
+
+if [ -z "$MONGO_URL" ]; then
+  echo "ERROR: Please set the MONGO_URL environment variable."
+  exit 1
+fi
+
 echo "module.exports = { db: '$MONGO_URL' }" > config.js
 
 node server.js
